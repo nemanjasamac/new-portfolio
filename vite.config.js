cd4fbbbca97726f145e378145f87b3f7481a0092
Nemanja Samac <samac.dev@gmail.com>
2025-11-26 02:22:07 +0100

    Vue Devtools

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools(),],
   assetsInclude: ['**/*.glb'],
 })
